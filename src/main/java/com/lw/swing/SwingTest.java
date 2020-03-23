6184c109164bdb1a3cfffcbc6bc9c4cf7573f7bf
Laowang2018 <wsj6051@126.com>
2020-03-24 00:24:33 +0800

    add swing test

---
+++ src/main/java/com/lw/swing/SwingTest.java
@@ -0,0 +1,25 @@
+package com.lw.swing;
+
+import com.lw.swing.jframe.CommonJframe;
+
+import javax.swing.*;
+
+public class SwingTest {
+
+
+    public static void main (String[] args) throws Exception{
+
+
+        JFrame jf = new CommonJframe();
+
+
+
+        for(int i=0; i<10; i++) {
+            Thread.sleep(3000);
+            System.out.println(i);
+        }
+
+
+
+    }
+}
---
+++ src/main/java/com/lw/swing/jframe/CommonJframe.java
@@ -0,0 +1,19 @@
+package com.lw.swing.jframe;
+
+import javax.swing.*;
+
+public class CommonJframe extends JFrame {
+    public CommonJframe() {
+        this.setSize(400, 400);
+        this.setLocation(300, 200);
+
+        /**
+         * EXIT_ON_CLOSE 窗体关闭时退出程序 :点关闭窗口时退出程序
+         * DISPOSE_ON_CLOSE 程序退出时销毁窗口:点击关闭时窗口并不会销毁,而是把下面的程序走完再销毁,窗口只是不显示了而已
+         * HIDE_ON_CLOSE 关闭时隐藏窗体:点关闭窗口时 不会销毁 而是直接隐藏
+         * DO_NOTHING_ON_CLOSE 就是点击关闭窗口时什么也不干,窗口也不销毁
+         */
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.setVisible(true);
+    }
+}
